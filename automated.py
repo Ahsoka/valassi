0df393b111cccf5bc3c9dffc7bb364c31615e51c
Ryan Lee <ryanlee619@gmail.com>
2022-01-28 16:39:27 -0800

    Created preliminary version of `buy_items` function

--- automated.py
+++ automated.py
@@ -9,4 +9,50 @@ def open_sealed_caches(number: int):
         key_press('space', 0.1)
 
 def buy_items(number_of_items: int):
-    raise NotImplementedError()
+    for count in range(22):
+        print(count)
+        time.sleep(0.5)
+        key_press("f", .5)
+
+        # time.sleep(1.5)
+        for i in range(45):
+            time.sleep(0.00001)
+            print(i)
+            key_press("space")
+            time.sleep(0.5)
+            key_press("space")
+
+        key_press("ESC")
+
+        time.sleep(0.5)
+
+        key_press("b")
+        time.sleep(0.5)
+
+        time.sleep(0.5)
+        key_press("s")
+        time.sleep(0.000001)
+        key_press("s")
+        time.sleep(0.000001)
+        key_press("enter")
+        time.sleep(0.5)
+
+        key_press("s")
+        time.sleep(0.000001)
+        for i in range(45):
+            # print(i)
+            key_press("v")
+            time.sleep(0.000001)
+
+        key_press("x", .1)
+
+        time.sleep(0.000001)
+
+        key_press("space")
+        time.sleep(0.000001)
+        key_press("ESC")
+        time.sleep(0.000001)
+        key_press("ESC")
+        time.sleep(0.000001)
+        key_press("ESC")
+        time.sleep(0.000001)
