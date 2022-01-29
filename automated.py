e5a35e456bc70b387df89d5b0bffc36f5cdd9c4e
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-01-28 17:32:52 -0800

    Improved reliability of buy_items function

--- automated.py
+++ automated.py
@@ -1,4 +1,5 @@
-from send_keys import key_press
+from send_keys import key_press, context_key_press
+import mouse
 import time
 
 def open_sealed_caches(number: int):
@@ -8,24 +9,23 @@ def open_sealed_caches(number: int):
         time.sleep(2)
         key_press('space', 0.1)
 
-def buy_items(num_of_runs, num_of_items):
-    for count in range(num_of_runs):
-        print(count)
+def buy_items():
+    NUM_OF_ITEMS_TO_BUY = 60
+    TOTAL_RUNS = 2
+
+    for count in range(TOTAL_RUNS):
         time.sleep(0.5)
         key_press("f", .5)
+        print(count)
 
-        # choose which item to buy based on arrow key
-        item_range = 6
-        for i in range(item_range):
-            time.sleep(0.000001)
-            key_press("s")
+        key_press('s', 2.5)
 
-        # time.sleep(1.5)
-        for i in range(num_of_items):
-            time.sleep(0.00001)
-            # print(i)
+        time.sleep(1.5)
+        for i in range(NUM_OF_ITEMS_TO_BUY):
+            time.sleep(0.1)
+            print(i)
             key_press("space")
-            time.sleep(0.5)
+            time.sleep(0.65)
             key_press("space")
 
         key_press("esc")
@@ -35,30 +35,21 @@ def buy_items(num_of_runs, num_of_items):
         key_press("b")
         time.sleep(0.5)
 
-        time.sleep(0.5)
-        key_press("s")
-        time.sleep(0.000001)
-        key_press("s")
-
-        # uncomment below for tool
-        time.sleep(0.000001)
-        key_press("s")
-
-
-        time.sleep(0.000001)
+        key_press('s', 1)
         key_press("enter")
         time.sleep(0.5)
 
-        # comment below out for tool
-        # key_press("s")
-        # time.sleep(0.000001)
+        with context_key_press('left-shift'):
+            mouse.move(2091, 414)
+            mouse.press()
+            mouse.move(2069, 851)
+            time.sleep(.5)
+            mouse.release()
+            mouse.move(1706, 780)
+            mouse.click()
+            time.sleep(.05)
 
-        for i in range(num_of_items):
-            # print(i)
-            key_press("v")
-            time.sleep(0.000001)
-
-        key_press("x", 1)
+        key_press("x", 1.2)
 
         time.sleep(0.000001)
 
