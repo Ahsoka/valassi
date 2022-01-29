73e69f46b0bee3aee826516c34b0aaac0f5b53f9
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-01-28 16:34:31 -0800

    Created `open_sealed_caches` function
    This function opens a given number of sealed caches

---
+++ automated.py
@@ -0,0 +1,9 @@
+from send_keys import key_press
+import time
+
+def open_sealed_caches(number: int):
+    for num in range(number):
+        print(f"Opening Cache {num + 1}")
+        key_press('f', 1.75)
+        time.sleep(2)
+        key_press('space', 0.1)
