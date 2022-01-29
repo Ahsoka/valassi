7f67116098c9ce53f9333ff02d5c8ad266c97617
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-01-28 18:22:50 -0800

    Added progress bar to `buy_items` function

--- automated.py
+++ automated.py
@@ -1,6 +1,7 @@
 from send_keys import key_press, context_key_press
 
 import mouse
+import tqdm
 import math
 import time
 
@@ -12,54 +13,54 @@ def open_sealed_caches(number: int):
         key_press('space', 0.1)
 
 def buy_items(num_of_items: int, bag_space: int = 30):
-    items = iter(range(num_of_items + 1))
-    TOTAL_RUNS = math.ceil(num_of_items / bag_space)
+    with tqdm.tqdm(total=num_of_items) as progress:
+        items = iter(range(num_of_items + 1))
+        TOTAL_RUNS = math.ceil(num_of_items / bag_space)
 
-    for count in range(TOTAL_RUNS):
-        time.sleep(0.5)
-        key_press("f", .5)
-        print(count)
+        for _ in range(TOTAL_RUNS):
+            time.sleep(0.5)
+            key_press("f", .5)
 
-        key_press('s', 2.5)
+            key_press('s', 2.5)
 
-        time.sleep(1.5)
-        for i, _ in zip(items, range(bag_space)):
-            time.sleep(0.1)
-            print(i)
+            time.sleep(1.5)
+            for i, _ in zip(items, range(bag_space)):
+                time.sleep(0.1)
+                key_press("space")
+                time.sleep(0.65)
+                key_press("space")
+                progress.update()
+
+            key_press("esc")
+
+            time.sleep(0.5)
+
+            key_press("b")
+            time.sleep(0.5)
+
+            key_press('s', 1)
+            key_press("enter")
+            time.sleep(0.5)
+
+            with context_key_press('left-shift'):
+                mouse.move(2165, 422)
+                mouse.press()
+                mouse.move(2150, 831)
+                time.sleep(.5)
+                mouse.release()
+                mouse.move(1706, 780)
+                mouse.click()
+                time.sleep(.05)
+
+            key_press("x", 1.2)
+
+            time.sleep(0.000001)
+
             key_press("space")
-            time.sleep(0.65)
-            key_press("space")
-
-        key_press("esc")
-
-        time.sleep(0.5)
-
-        key_press("b")
-        time.sleep(0.5)
-
-        key_press('s', 1)
-        key_press("enter")
-        time.sleep(0.5)
-
-        with context_key_press('left-shift'):
-            mouse.move(2091, 414)
-            mouse.press()
-            mouse.move(2069, 851)
-            time.sleep(.5)
-            mouse.release()
-            mouse.move(1706, 780)
-            mouse.click()
-            time.sleep(.05)
-
-        key_press("x", 1.2)
-
-        time.sleep(0.000001)
-
-        key_press("space")
-        time.sleep(0.000001)
-        key_press("esc")
-        time.sleep(0.000001)
-        key_press("esc")
-        time.sleep(0.000001)
-        key_press("esc")
-        time.sleep(0.000001)
+            time.sleep(0.000001)
+            key_press("esc")
+            time.sleep(0.000001)
+            key_press("esc")
+            time.sleep(0.000001)
+            key_press("esc")
+            time.sleep(0.000001)
